63a069f6af0e5935b36d0e28d3697ecbb7260bdb
Keira-McLean_lmig <keira.mclean@libertymutual.com>
2023-04-26 07:56:35 -0400

    add new questions

--- javascript/main.js
+++ javascript/main.js
@@ -16,8 +16,8 @@ function buildQuiz() {
                 );
             }
             output.push(
-                `<div class="question"> ${currentQuestion.question} </div>
-                    <div class="answers"> ${choices.join('')} </div>`
+                `<div class="question"> ${currentQuestion.question} ${currentQuestion.image} </div>
+                    <div class="answers"> ${choices.join('')}<br><br></div>`
             );
         }
     );
@@ -29,6 +29,7 @@ function showResults() {
     const answerResults = quiz.querySelectorAll('.answers');
 
     let numCorrect = 0;
+    let numIncorrect = 0;
 
     questions.forEach ((currentQuestion, questionNumber) => {
         const answerResult = answerResults[questionNumber];
@@ -41,9 +42,11 @@ function showResults() {
         answerResults[questionNumber].style.color = 'lightgreen';
         } else {
             answerResults[questionNumber].style.color = 'red';
+            numIncorrect++;
         }
     });
-    results.innerHTML = `${numCorrect} out of ${questions.length}`;
+    results.innerHTML = `${numCorrect} out of ${questions.length} Correct and ${numIncorrect} wrong`;
+    r
 }
 
 const quiz = document.getElementById("quiz");
@@ -52,24 +55,123 @@ const results = document.getElementById("results");
 
 const questions = [
     {
-        question: "What is CSS?",
+        question: 'What is the piece of plastic covering at the ends of your shoelace, so you don’t have to moisten them with spit to thread them through your shoelace holes?',
         choices: {
-            a: "Cascading Style Sheets",
-            b: "Crazy Sarcastic Sister",
-            c: "Create Stylish System",
-            d: "Container System Style"
+            a: "Langyard",
+            b: "Aglet",
+            c: "Eylet",
+            d: "Puggle"
         },
-        correct: "a"
+        correct: "b",
+        image: "<img src='/images/shoelace.jpg'>"
     },
     {
-        question: "What is HTML?",
+        question: "What are the posts in the parking lot that lets wheelchairs and shopping carts go through, but not your car?",
         choices: {
-            a: "Huge Text Markup Language",
-            b: "HyperText Markup Language",
-            c: "Hard To Melt Liquid",
-            d: "Help Text Multi Language"
+            a: "Bollard",
+            b: "Lenker",
+            c: "Dike",
+            d: "Liripipe"
         },
-        correct: "b"
+        correct: "a",
+        image: "<img src='/images/steelpost.jpg'>"
+
+    },
+    {
+        question: 'What are the non-alphanumeric, non-punctuation characters, usually used when you want to write something that you don’t want your children to read?',
+        choices: {
+            a: "Wing-ding",
+            b: "Tarnmark",
+            c: "Dingbat",
+            d: "Gudgeon"
+        },
+        correct: "a",
+        image: "<img src='/images/junkchars.jpg'>"
+
+    },
+    {
+        question: 'What is the metal band that connects the pencil eraser to the end of the pencil?',
+        choices: {
+            a: "Nesh",
+            b: "Crottle",
+            c: "Tiffin",
+            d: "Furrule"
+        },
+        correct: "d",
+        image: "<img src='/images/eraserthing.jpg'>"
+
+    },
+    {
+        question: 'What is the leather loop in your belt or watch strap that keeps the end in place after it has been fastened through the buckle?',
+        choices: {
+            a: "Keeper",
+            b: "Shofar",
+            c: "Cupre",
+            d: "Wittol"
+        },
+        correct: "a",
+        image: "<img src='/images/bucklethingy.jpg'>"
+
+    },
+    {
+        question: 'What is the groove made by a sawblade, ie, the width of a cut?',
+        choices: {
+            a: "Dwal",
+            b: "Tiffin",
+            c: "Marlet",
+            d: "Kerf"
+        },
+        correct: "d",
+        image: "<img src='/images/cutseam.jpg'>"
+
+    },
+    {
+        question: 'What is the small indentation at the bottom of a wine bottle, designed to give the bottle extra strength (so they say....), and also, to make it look like it has more wine than it really does (yeah, I hate that.....)?',
+        choices: {
+            a: "Winebibber",
+            b: "Thrutch",
+            c: "Punt",
+            d: "Mim"
+        },
+        correct: "c",
+        image: "<img src='/images/bottlebottom.jpg'>"
+
+    },
+    {
+        question: 'What is the vertical groove between your lip and nose that separates your left and right mustache, unless you’re Hitler, then it’s the part that your mustache covers?',
+        choices: {
+            a: "Sternutator",
+            b: "Philtrum",
+            c: "Mallam",
+            d: "Dariole"
+        },
+        correct: "b",
+        image: "<img src='/images/noselipgroove.jpg'>"
+
+    },
+    {
+        question: 'What are the points of light that you see behind your eyelids when you shut your eyes really hard?',
+        choices: {
+            a: "Famulus",
+            b: "Flocculents",
+            c: "Seraks",
+            d: "Phosphenes"
+        },
+        correct: "d",
+        image: "<img src='/images/eyestars.jpg'>"
+
+    },
+    {
+        question: 'What is that little piece of cartilage that sticks out at the front side of your ear?',
+        choices: {
+            a: "Tragus",
+            b: "Shallop",
+            c: "Spurr",
+            d: "Ensorcell"
+        },
+        correct: "a",
+        image: "<img src='/images/earthingy.jpg'>"
+
     }
 ];
 
