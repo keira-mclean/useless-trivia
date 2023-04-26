9f31bb7ef4d4ac1fcf67a63b49fbdaa7cc67be82
Keira-McLean_lmig <keira.mclean@libertymutual.com>
2023-04-26 11:13:01 -0400

    got pages working

--- javascript/main.js
+++ javascript/main.js
@@ -16,8 +16,10 @@ function buildQuiz() {
                 );
             }
             output.push(
-                `<div class="question"> ${currentQuestion.question} ${currentQuestion.image} </div>
-                    <div class="answers"> ${choices.join('')}<br><br></div>`
+                `<div class="slide">
+                <div class="question"> ${currentQuestion.question} ${currentQuestion.image} </div>
+                    <div class="answers"> ${choices.join('')}<br><br></div>
+                </div>`
             );
         }
     );
@@ -44,6 +46,31 @@ function showResults() {
     });
     results.innerHTML = `${numCorrect} out of ${questions.length} Correct and ${numIncorrect} wrong`;
     
+}
+
+function showSlide(n) {
+    slides[currentSlide].classList.remove('active-slide');
+    slides[n].classList.add('active-slide');
+    currentSlide = n;
+    if(currentSlide === 0) {
+        previousButton.style.display = 'none';
+    } else {
+        previousButton.style.display = 'inline-block';
+    }
+    if (currentSlide === slides.length-1) {
+        nextButton.style.display = 'none';
+        submit.style.display = 'inline-block';
+    } else {
+        nextButton.style.display = 'inline-block';
+        submit.style.display = 'none';
+    }
+}
+
+function showNextSlide() {
+    showSlide(currentSlide + 1);
+}
+function showPreviousSlide() {
+    showSlide(currentSlide - 1);
 }
 
 const quiz = document.getElementById("quiz");
@@ -174,6 +201,15 @@ const questions = [
 
 buildQuiz();
 
+const previousButton = document.getElementById("previous");
+const nextButton = document.getElementById("next");
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+showSlide(currentSlide);
+
 submit.addEventListener("click", showResults);
+previousButton.addEventListener("click", showPreviousSlide);
+nextButton.addEventListener("click", showNextSlide);
 
 })();
